1a45e41c0f60f836b8a13242dd7de33048d260e0
josesoaresif <josesoares@ifthenpay.com>
2022-03-28 15:08:51 +0000

    tests: test commit message

---
+++ opencart versão 3/admin/language/pt-pt/extension/payment/ccard.php
@@ -0,0 +1,70 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'Cartão de Crédito';
+$_['heading_title_multibanco'] = 'Cartão de Crédito Configurações';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_payment'] = 'Pagamento';
+$_['text_success'] = 'Success: You have modified Credit Card payment module!';
+$_['text_ccard'] = '<a onclick="window.open(\'https:www.ifthenpay.com\');"><img src="view/image/payment/ifthenpay/ccard.svg" class="ccardLogo" alt="Credit Card Logo" title="Credit Card" style="border: 1px solid #EEEEEE; max-width: 35%;" /><br /></a>';
+$_['acess_user_documentation'] = 'Aceder ao Manual de Utilizador.';
+$_['create_account_now'] = 'Crie uma conta agora!';
+$_['text_home'] = 'Home';
+$_['text_all_zones'] = 'Todas as regiões';
+
+//Entry
+$_['entry_backofficeKey'] = 'Chave de acesso ao backoffice';
+$_['text_enabled'] = 'Ativo';
+$_['text_disabled'] = 'Desativado';
+$_['add_new_accounts'] = 'Adicionou novas contas contrato?';
+$_['reset_accounts'] = 'Reset Contas';
+$_['sandbox_help'] = 'Ative o modo sandbox, para poder testar o módulo sem ativar o callback.';
+$_['sandbox_mode'] = 'Modo Sandbox';
+$_['reset_account_success'] = 'Conta Ifthenpay reinicializada com sucesso!';
+$_['reset_account_error'] = 'Erro a reinicializar conta Ifthenpay!';
+$_['dontHaveAccount_ccard'] = 'Não tem conta Cartão de Crédito?';
+$_['requestAccount_ccard'] = 'Solicitar criação de conta Cartão de Crédito';
+$_['newUpdateAvailable'] = 'Nova atualização disponível!';
+$_['moduleUpToDate'] = 'O módulo está atualizado!';
+$_['downloadUpdateModule'] = 'Download Update Módulo';
+$_['acess_user_documentation_link'] = 'https://www.ifthenpay.com/downloads/opencart/opencart_user_guide_pt.pdf';
+$_['entry_minimum_value'] = 'Valor Mínimo da Encomenda';
+$_['activate_cancelCcardOrder'] = 'Cancelar Encomenda Cartão de Crédito';
+$_['ccardOrderCancel_help'] = 'Cancele a encomenda Cartão de Crédito após os dados de pagamento  expirarem.';
+
+
+// Entry
+$_['activate_callback'] = 'Ativar Callback';
+$_['switch_enable'] = 'Ativar';
+$_['switch_disable'] = 'Desativar';
+$_['entry_order_status'] = 'Estado da Encomenda:';
+$_['entry_order_status_complete'] = 'Estado da Encomenda Pago:';
+$_['entry_order_status_canceled'] = 'Estado da Encomenda Cancelado:';
+$_['entry_order_status_failed'] = 'Estado da Encomenda Falhou:';
+$_['entry_geo_zone'] = 'Geo Zone:';
+$_['entry_status'] = 'Estados:';
+$_['entry_sort_order'] = 'Ordem do Método de Pagamento:';
+$_['entry_ccard_ccardKey'] = 'CCard Key';
+$_['choose_entity'] = 'Escolha a Entidade';
+$_['entry_antiPhishingKey'] = 'Chave Anti-Phishing';
+$_['entry_urlCallback'] = 'Url de Callback';
+$_['callbackIsActivated'] = 'Callback ativado';
+$_['callbackNotActivated'] = 'Callback não ativado';
+$_['show_paymentMethod_logo'] = 'Mostrar o Logotipo do Método de Pagamento no Checkout';
+$_['request_new_account_success'] = 'Email a solicitar nova conta enviado com sucesso.';
+$_['request_new_account_error'] = 'Erro ao enviar email a solicitar nova conta.';
+$_['text_cron_1'] = 'Cron Job\'s são tarefas agendadas e executadas periodicamente. Para configurar o seu servidor, pode ler a <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link"> documentação do opencart </ a >';
+$_['text_cron_2'] = 'Precisa definir o Cron para ser executado a cada minuto.';
+$_['text_cron_3'] = 'Quando o callback falhar pode definir um cron job para consultar o estado do pagamento.';
+$_['text_cron_2'] = 'Precisa definir o Cron para ser executado todos os dias à 1h:00m.';
+$_['entry_cron'] = 'Cron URL';
+$_['button_copy'] = 'Copy';
+$_['text_instruction'] = 'CRON Instruções';
+
+// Error
+$_['error_permission'] = 'Aviso: Não tem permissão para modificar o módulo MB WAY!';
+$_['error_backofficeKey_required'] = 'Chave de acesso ao backoffice é obrigatória!';
+$_['error_backofficeKey_error'] = 'Erro a salvar a chave de acesso ao backoffice!';
+?>
